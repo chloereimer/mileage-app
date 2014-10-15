396d64eb4d22add5f8cc176f05b255762b7b63b4
Chloe Reimer <chloe.reimer@gmail.com>
2014-10-15 12:40:15 -0400

    adds getCars() method to main managed bean

--- src/java/com/mohawkcollege/cr/MileageApp.java
+++ src/java/com/mohawkcollege/cr/MileageApp.java
@@ -5,8 +5,14 @@
  */
 package com.mohawkcollege.cr;
 
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.annotation.Resource;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 /**
  *
@@ -15,11 +21,38 @@ import javax.faces.bean.SessionScoped;
 @ManagedBean
 @SessionScoped
 public class MileageApp {
+    @PersistenceContext(unitName = "mileage_appPU")
+    private EntityManager em;
+    @Resource
+    private javax.transaction.UserTransaction utx;
 
     /**
      * Creates a new instance of MileageApp
      */
     public MileageApp() {
+    }
+
+    public void persist(Object object) {
+        try {
+            utx.begin();
+            em.persist(object);
+            utx.commit();
+        } catch (Exception e) {
+            Logger.getLogger(getClass().getName()).log(Level.SEVERE, "exception caught", e);
+            throw new RuntimeException(e);
+        }
+    }
+    
+    /**
+     * Obtains a list of all cars in the "Cars" table
+     * @return a list of all cars
+     * 
+     */
+    public List<Cars> getCars() {
+        // Note : You do not need to specify the query class
+        List<Cars> allCars = (List<Cars>) (em.createNamedQuery("Cars.findAll").         
+            getResultList());
+        return allCars; 
     }
     
 }
